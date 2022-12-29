7fe3ee87f5a296576b103dc2dc05f5a95fce19dc
Volodia Fernandez <vfz1204@gmail.com>
2022-12-30 00:26:40 +0100

    Fixed warnings

--- src/routes/game/(main_menu)/+layout.svelte
+++ src/routes/game/(main_menu)/+layout.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import type { LayoutData } from './$types';
-
-	export let data: LayoutData;
 
 	let current_page = 'home';
 
--- src/routes/game/+layout.svelte
+++ src/routes/game/+layout.svelte
@@ -1,7 +1,1 @@
-<script lang="ts">
-	import type { LayoutData } from './$types';
-
-	export let data: LayoutData;
-</script>
-
 <slot />
